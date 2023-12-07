73bdbb22d60f2c0d4c4282e6e7d5789d43536683
Thom Hopmans <thom@floryn.com>
2023-12-07 13:34:20 +0100

    Refactor day 7

--- 2023/adventofcode/exercises/exercise7.rb
+++ 2023/adventofcode/exercises/exercise7.rb
@@ -3,14 +3,15 @@ require_relative 'helpers/range'
 class Hand
   attr_reader :hand, :bid, :rank_name
 
-  def initialize(hand, bid)
+  def initialize(hand, bid, jokers: false)
     @hand = hand
     @bid = bid.to_i
+    @jokers = jokers
     @rank_id = nil
     @rank_name = nil
     @rank_cards = nil
 
-    score
+    calculate_score
   end
 
   def to_s
@@ -25,28 +26,39 @@ class Hand
     value.each_with_index do |value, index|
       comparison_result = value <=> other.value[index]
 
-      if comparison_result == 1
-        return 1
-      elsif comparison_result == -1
-        return -1
-      end
+      return comparison_result if [-1, 1].include?(comparison_result)
     end
   end
 
-  def score
+  def calculate_score
     # Count occurrences of each rank
     @rank_cards = hand.chars
       .map { |element| element == 'A' ? '14' : element }
       .map { |element| element == 'K' ? '13' : element }
       .map { |element| element == 'Q' ? '12' : element }
-      .map { |element| element == 'J' ? '11' : element }
       .map { |element| element == 'T' ? '10' : element }
+
+    @rank_cards = if @jokers
+      @rank_cards.map { |element| element == 'J' ? '1' : element }
+    else
+      @rank_cards.map { |element| element == 'J' ? '11' : element }
+    end
 
     rank_counts = Hash.new(0)
     @rank_cards.each { |card| rank_counts[card.to_i] += 1 }
 
     # Check for various hands
     sorted_hash = rank_counts.sort_by { |key, value| [-value, -key] }.to_h
+    n_jokers = sorted_hash[1] || 0
+
+    if n_jokers > 0
+      score_hand_with_jokers(sorted_hash, n_jokers)
+    else
+      score_hand_with_no_jokers(sorted_hash)
+    end
+  end
+
+  def score_hand_with_no_jokers(sorted_hash)
     case sorted_hash.values
       when [5]
         @rank_id = 7
@@ -73,117 +85,32 @@ class Hand
         raise
     end
   end
-end
 
-class JokerHand
-  attr_reader :hand, :bid, :rank_name
-
-  def initialize(hand, bid)
-    @hand = hand
-    @bid = bid.to_i
-    @rank_id = nil
-    @rank_name = nil
-    @rank_cards = nil
-
-    score
-  end
-
-  def to_s
-    "#{@hand} #{bid} #{@rank_name} #{value} \n"
-  end
-
-  def value
-    [@rank_id] + @rank_cards.map(&:to_i)
-  end
-
-  def <=>(other)
-    value.each_with_index do |value, index|
-      comparison_result = value <=> other.value[index]
-
-      if comparison_result == 1
-        return 1
-      elsif comparison_result == -1
-        return -1
-      end
-    end
-  end
-
-  def score
-    # Count occurrences of each rank
-    @rank_cards = hand.chars
-      .map { |element| element == 'A' ? '14' : element }
-      .map { |element| element == 'K' ? '13' : element }
-      .map { |element| element == 'Q' ? '12' : element }
-      .map { |element| element == 'J' ? '1' : element }
-      .map { |element| element == 'T' ? '10' : element }
-
-    rank_counts = Hash.new(0)
-    @rank_cards.each { |card| rank_counts[card.to_i] += 1 }
-
-    # Check for various hands
-    sorted_hash = rank_counts.sort_by { |key, value| [-value, -key] }.to_h
-    n_jokers = sorted_hash[1] || 0
-
-    if n_jokers > 0
-
-      case sorted_hash.values
-        when [5]
-          @rank_id = 7
-          @rank_name = 'Five of a Kind'
-        when [4, 1]
-          @rank_id = 7
-          @rank_name = 'Five of a Kind'
-        when [3, 2]
-          @rank_id = 7
-          @rank_name = 'Five of a Kind'
-        when [3, 1, 1]
-          @rank_id = 6
-          @rank_name = 'Four of a Kind'
-        when [2, 2, 1]
-          case n_jokers
-            when 2
-              @rank_id = 6
-              @rank_name = 'Four of a Kind'
-            when 1
-              @rank_id = 5
-              @rank_name = 'Full House'
-          end
-        when [2, 1, 1, 1]
-          @rank_id = 4
-          @rank_name = 'Three of a Kind'
-        when [1, 1, 1, 1, 1]
-          @rank_id = 2
-          @rank_name = 'One Pair'
-        else
-          raise
-      end
-
-    else
-      case sorted_hash.values
-        when [5]
-          @rank_id = 7
-          @rank_name = 'Five of a Kind'
-        when [4, 1]
-          @rank_id = 6
-          @rank_name = 'Four of a Kind'
-        when [3, 2]
-          @rank_id = 5
-          @rank_name = 'Full House'
-        when [3, 1, 1]
-          @rank_id = 4
-          @rank_name = 'Three of a Kind'
-        when [2, 2, 1]
-          @rank_id = 3
-          @rank_name = 'Two Pair'
-        when [2, 1, 1, 1]
-          @rank_id = 2
-          @rank_name = 'One Pair'
-        when [1, 1, 1, 1, 1]
-          @rank_id = 1
-          @rank_name = 'High Card'
-        else
-          raise
-      end
+  def score_hand_with_jokers(sorted_hash, n_jokers)
+    case sorted_hash.values
+      when [5], [4, 1], [3, 2]
+        @rank_id = 7
+        @rank_name = 'Five of a Kind'
+      when [3, 1, 1]
+        @rank_id = 6
+        @rank_name = 'Four of a Kind'
+      when [2, 2, 1]
+        case n_jokers
+          when 2
+            @rank_id = 6
+            @rank_name = 'Four of a Kind'
+          when 1
+            @rank_id = 5
+            @rank_name = 'Full House'
+        end
+      when [2, 1, 1, 1]
+        @rank_id = 4
+        @rank_name = 'Three of a Kind'
+      when [1, 1, 1, 1, 1]
+        @rank_id = 2
+        @rank_name = 'One Pair'
+      else
+        raise
     end
   end
 end
@@ -197,8 +124,8 @@ class Exercise7
 
   def run_a(instructions)
     hands = instructions.split("\n").map do |line|
-      line = line.split(' ')
-      Hand.new(line[0], line[1])
+      line = line.split
+      Hand.new(line[0], line[1], jokers: false)
     end
 
     # 253638586
@@ -207,8 +134,8 @@ class Exercise7
 
   def run_b(instructions)
     hands = instructions.split("\n").map do |line|
-      line = line.split(' ')
-      JokerHand.new(line[0], line[1])
+      line = line.split
+      Hand.new(line[0], line[1], jokers: true)
     end
 
     # 253253225
--- 2023/adventofcode/exercises/exercise7_test.rb
+++ 2023/adventofcode/exercises/exercise7_test.rb
@@ -2,37 +2,37 @@ require 'minitest/autorun'
 require_relative 'exercise7'
 
 class Exercise6Test < Minitest::Test
-  #   def test_example_a
-  #     assert_equal 6440, Exercise7.new.run_a("32T3K 765
-  # T55J5 684
-  # KK677 28
-  # KTJJT 220
-  # QQQJA 483")
-  #   end
+  def test_example_a
+    assert_equal 6440, Exercise7.new.run_a("32T3K 765
+  T55J5 684
+  KK677 28
+  KTJJT 220
+  QQQJA 483")
+  end
 
-  #   def test_example_a_2
-  #     assert_equal 2182, Exercise7.new.run_a("AKATA 640
-  #       QJAAA 902")
-  #   end
+  def test_example_a_2
+    assert_equal 2182, Exercise7.new.run_a("AKATA 640
+        QJAAA 902")
+  end
 
-  #   def test_example_a_3
-  #     assert_equal 35, Exercise7.new.run_a("33332 5
-  #       2AAAA 25")
-  #   end
+  def test_example_a_3
+    assert_equal 35, Exercise7.new.run_a("33332 5
+        2AAAA 25")
+  end
 
-  #   def test_example_b
-  #     assert_equal 5905, Exercise7.new.run_b("32T3K 765
-  # T55J5 684
-  # KK677 28
-  # KTJJT 220
-  # QQQJA 483")
-  #   end
+  def test_example_b
+    assert_equal 5905, Exercise7.new.run_b("32T3K 765
+  T55J5 684
+  KK677 28
+  KTJJT 220
+  QQQJA 483")
+  end
 
-  def test_hands
-    assert_equal 'Three of a Kind', JokerHand.new('JJ548', '0').rank_name
-    assert_equal 'Five of a Kind', JokerHand.new('KJKJK', '0').rank_name
-    assert_equal 'Four of a Kind', JokerHand.new('AJ888', '0').rank_name
-    assert_equal 'Full House', JokerHand.new('3939J', '0').rank_name
-    assert_equal 'Five of a Kind', JokerHand.new('JJ8JJ', '0').rank_name
+  def test_hands_with_jokers
+    assert_equal 'Three of a Kind', Hand.new('JJ548', '0', jokers: true).rank_name
+    assert_equal 'Five of a Kind', Hand.new('KJKJK', '0', jokers: true).rank_name
+    assert_equal 'Four of a Kind', Hand.new('AJ888', '0', jokers: true).rank_name
+    assert_equal 'Full House', Hand.new('3939J', '0', jokers: true).rank_name
+    assert_equal 'Five of a Kind', Hand.new('JJ8JJ', '0', jokers: true).rank_name
   end
 end

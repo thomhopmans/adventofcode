9afd7baed467701246ccd71bd36d1a2dc0403471
Thom Hopmans <thom@floryn.com>
2023-12-10 20:54:30 +0100

    Add 10B

--- 2023/adventofcode/exercises/exercise10.rb
+++ 2023/adventofcode/exercises/exercise10.rb
@@ -1,22 +1,18 @@
 require_relative 'helpers/exercise'
 
+NORTH = [-1, 0].freeze
+EAST = [0, 1].freeze
+SOUTH = [1, 0].freeze
+WEST = [0, -1].freeze
+
 class Exercise10 < Exercise
   EXERCISE_NUMBER = 10
 
-  DIRECTIONS = [
-    [-1, 0], # North
-    [0, 1], # East
-    [1, 0], # South
-    [0, -1], # West
-  ].freeze
+  DIRECTIONS = [NORTH, EAST, SOUTH, WEST].freeze
   PIPES_TO_NORTH = ['|', 'L', 'J', 'S'].freeze
-  PIPES_FROM_NORTH = ['|', '7', 'F'].freeze
   PIPES_TO_EAST = ['-', 'F', 'L', 'S'].freeze
-  PIPES_FROM_EAST = ['-', '7', 'J'].freeze
   PIPES_TO_SOUTH = ['|', 'F', '7', 'S'].freeze
-  PIPES_FROM_SOUTH = ['|', 'L', 'J'].freeze
   PIPES_TO_WEST = ['-', '7', 'J', 'S'].freeze
-  PIPES_FROM_WEST = ['-', 'L', 'F'].freeze
 
   def run
     puts "Exercise #{self.class::EXERCISE_NUMBER}:"
@@ -27,9 +23,23 @@ class Exercise10 < Exercise
   def run_a(data)
     grid = to_grid(data)
     position_s = find_character(grid, 'S')
-    grid[position_s[0]][position_s[1]] = s_pipe_character(grid, position_s)
+    grid = replace_s_character_with_pipe(grid, position_s)
 
-    # Depth first search
+    path = find_path(grid, position_s)
+    (path.size + 1) / 2
+  end
+
+  def run_b(data)
+    grid = to_grid(data)
+    position_s = find_character(grid, 'S')
+    grid = replace_s_character_with_pipe(grid, position_s)
+    path = find_path(grid, position_s)
+
+    enclosed(grid, path)
+  end
+
+  def find_path(grid, position_s)
+    # Depth first search to find path
     # Note: storing paths makes search a bit slower, but is required for part B
     @candidate_stack = [[position_s, 0, [position_s]]]
     @visited = Set.new
@@ -43,40 +53,45 @@ class Exercise10 < Exercise
       step(grid, position_s, candidate)
     end
 
-    longest_path = @path_lengths.max_by { |array| array.first }
-    (longest_path.first + 1) / 2
+    @path_lengths.max_by(&:first)[1]
   end
 
-  def step(grid, position_s, traverse_path)
-    @visited.add(traverse_path[0])
+  def step(grid, position_s, path)
+    @visited.add(path[0])
 
-    new_positions = connected_directions(grid, traverse_path[0])
-      .map { elem_addition(traverse_path[0], _1) }
-
+    new_positions = next_points(grid, path[0])
     if new_positions.include?(position_s)
-      @path_lengths.append([traverse_path[1], traverse_path[2]])
+      @path_lengths.append([path[1], path[2]])
     end
 
-    new_positions = new_positions.reject { traverse_path.include?(_1) && _1 != position_s }
-
+    new_positions = new_positions.reject { path.include?(_1) && _1 != position_s }
     if new_positions.empty?
       return
     end
 
     new_positions.map do |new_pos|
-      @candidate_stack << [new_pos, traverse_path[1] + 1, traverse_path[2] + [new_pos]]
+      @candidate_stack << [new_pos, path[1] + 1, path[2] + [new_pos]]
     end
   end
 
-  def run_b(_data)
-    0
+  def enclosed(grid, longest_path)
+    (0...grid.length).map do |y|
+      inside = false # Assuming left most point is outside
+      (0...grid.first.length).map do |x|
+        # Every time we hit a pipe ON the path, we move either out->in or in>out
+        if '|JLS'.chars.include?(grid[y][x]) && longest_path.include?([y, x])
+          inside = !inside
+        end
+
+        # Only enclosed when not on path
+        next unless inside && !longest_path.include?([y, x])
+
+        1
+      end.flatten
+    end.flatten.compact.sum
   end
 
   private
-
-  def elem_addition(array1, array2)
-    array1.zip(array2).map { |a, b| a + b }
-  end
 
   def to_grid(data)
     data.split("\n").map(&:chars)
@@ -97,43 +112,61 @@ class Exercise10 < Exercise
     nil
   end
 
-  def s_pipe_character(grid, position)
-    case connected_directions(grid, position)
-      when [[-1, 0], [0, -1]]
+  def replace_s_character_with_pipe(grid, position)
+    adjacent_points = DIRECTIONS.map { |direction| elem_addition(position, direction) }
+    adjacent_chars = adjacent_points.map { |point| outside_grid(grid, point) ? '?' : grid[point[0]][point[1]] }
+    adjacent_connections = [
+      PIPES_TO_SOUTH.include?(adjacent_chars[0]),
+      PIPES_TO_WEST.include?(adjacent_chars[1]),
+      PIPES_TO_NORTH.include?(adjacent_chars[2]),
+      PIPES_TO_EAST.include?(adjacent_chars[3]),
+    ]
+    grid[position[0]][position[1]] = case adjacent_connections
+      when [true, false, false, true]
         'J'
-      when [[0, 1], [1, 0]]
+      when [false, true, true, false]
         'F'
+      when [false, false, true, true]
+        '7'
+      when [true, true, false, false]
+        'L'
+      when [true, false, true, false]
+        '|'
+      when [false, true, false, true]
+        '-'
       else
-        # Not implemented
         raise
     end
+    grid
   end
 
-  # rubocop: disable Metrics/PerceivedComplexity
-  def connected_directions(grid, position)
-    DIRECTIONS.map.with_index do |direction, index|
-      new_position = elem_addition(position, direction)
-
-      if position_outside_grid(grid, new_position)
-        nil
-      else
-        current = grid[position[0]][position[1]]
-        other = grid[new_position[0]][new_position[1]]
-
-        is_connected_north = index == 0 && PIPES_TO_NORTH.include?(current) && PIPES_FROM_NORTH.include?(other)
-        is_connected_east = index == 1 && PIPES_TO_EAST.include?(current) && PIPES_FROM_EAST.include?(other)
-        is_connected_south = index == 2 && PIPES_TO_SOUTH.include?(current) && PIPES_FROM_SOUTH.include?(other)
-        is_connected_west = index == 3 && PIPES_TO_WEST.include?(current) && PIPES_FROM_WEST.include?(other)
-
-        direction if is_connected_north || is_connected_east || is_connected_south || is_connected_west
-      end
-    end.compact
+  def next_points(grid, position)
+    current = grid[position[0]][position[1]]
+    case current
+      when 'J'
+        [DIRECTIONS[0], DIRECTIONS[3]]
+      when 'L'
+        [DIRECTIONS[0], DIRECTIONS[1]]
+      when 'F'
+        [DIRECTIONS[1], DIRECTIONS[2]]
+      when '7'
+        [DIRECTIONS[2], DIRECTIONS[3]]
+      when '|'
+        [DIRECTIONS[0], DIRECTIONS[2]]
+      when '-'
+        [DIRECTIONS[1], DIRECTIONS[3]]
+    end
+      .map { |direction| elem_addition(position, direction) }
+      .filter_map { |pos| pos unless outside_grid(grid, pos) }
   end
-  # rubocop: enable Metrics/PerceivedComplexity
 
-  def position_outside_grid(grid, position)
+  def elem_addition(array1, array2)
+    array1.zip(array2).map { |a, b| a + b }
+  end
+
+  def outside_grid(grid, point)
     rows = grid.length
     columns = grid.first.length
-    position[0] < 0 || position[0] >= rows || position[1] < 0 || position[1] >= columns
+    point[0] < 0 || point[0] >= rows || point[1] < 0 || point[1] >= columns
   end
 end
--- 2023/adventofcode/tests/exercise10_test.rb
+++ 2023/adventofcode/tests/exercise10_test.rb
@@ -29,4 +29,58 @@ LJ--J")
   def test_input_a
     assert_equal 6927, Exercise10.new.run_a(Exercise10.new.load_data)
   end
+
+  def test_example_b_one
+    assert_equal 4, Exercise10.new.run_b("...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........")
+  end
+
+  def test_example_b_two
+    assert_equal 4, Exercise10.new.run_b("..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........")
+  end
+
+  def test_example_b_three
+    assert_equal 8, Exercise10.new.run_b(".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...")
+  end
+
+  def test_example_b_four
+    assert_equal 10, Exercise10.new.run_b("FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L")
+  end
+
+  def test_input_b
+    assert_equal 467, Exercise10.new.run_b(Exercise10.new.load_data)
+  end
 end

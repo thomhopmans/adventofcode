c1f59b0f6ef2728e9e7eb2719069043ba9e4f0b3
Thom Hopmans <thom@floryn.com>
2023-12-20 23:00:54 +0100

    Refactor

--- 2023/adventofcode/exercises/exercise20.rb
+++ 2023/adventofcode/exercises/exercise20.rb
@@ -13,10 +13,10 @@ class Exercise20 < Exercise
   end
 
   def run_a(data)
-    flip_flops, conjunctions, module_map = parse_cables(data)
+    flip_flops, conjunctions = parse_cables(data)
 
     output = (1..1000).map do |iteration|
-      push_button(iteration, module_map, flip_flops, conjunctions)
+      push_button(iteration, flip_flops, conjunctions)
     end
 
     output_a, output_b = output.transpose.map(&:sum)
@@ -30,11 +30,11 @@ class Exercise20 < Exercise
   # > For each of those 4 conjunction, it must hold that:
   #   memory inflow is not all high pulses, because then it will send a high pulse to kh
   def run_b(data)
-    flip_flops, conjunctions, module_map = parse_cables(data)
+    flip_flops, conjunctions = parse_cables(data)
 
     # 4080 is exactly sufficient for my input
     (1..4080).map do |iteration|
-      push_button(iteration, module_map, flip_flops, conjunctions)
+      push_button(iteration, flip_flops, conjunctions)
     end
 
     conjunctions_to_kh = ['pv', 'qh', 'xm', 'hz'].freeze
@@ -50,96 +50,80 @@ class Exercise20 < Exercise
   def parse_cables(data)
     flip_flops = {}
     conjunctions = {}
-    module_map = {}
+    @module_map = {}
 
     data.split("\n").map do |line|
-      t = line.split(' -> ')
-      [t[0], t[1].split(', ')]
-    end.each do |name, where|
+      name, where = line.split(' -> ')
+
       if name[0] == '%'
         flip_flops[name[1..-1]] = false
-        module_map[name[1..-1]] = where
+        @module_map[name[1..-1]] = where.split(', ')
         next
       end
 
       if name[0] == '&'
         conjunctions[name[1..-1]] = {}
-        module_map[name[1..-1]] = where
+        @module_map[name[1..-1]] = where.split(', ')
         next
       end
 
-      module_map[name] = where
+      @module_map[name] = where.split(', ')
     end
 
-    conjunctions.keys.each do |c|
-      module_map.each do |name, where|
+    conjunctions.each_key do |c|
+      @module_map.each do |name, where|
         conjunctions[c][name] = false if where.include?(c)
       end
     end
 
+    @module_map.freeze
     @conjunctions_first_high_pulse = Hash.new(0) # Helper for part B
-    [flip_flops, conjunctions, module_map]
+
+    [flip_flops, conjunctions] # State
   end
 
-  def push_button(iteration, module_map, flip_flops, conjunctions)
+  def push_button(iteration, flip_flops, conjunctions)
     queue = []
     a = 0
     b = 1
 
-    # Broadcast signal at start
-    module_map['broadcaster'].each do |w|
-      queue.push([w, LOW_PULSE, 'broadcaster'])
-    end
+    broadcast_initial_signal(queue)
 
     # Process all cables
     until queue.empty?
       to, impulse, from = queue.shift
 
-      if impulse == HIGH_PULSE
-        a += 1
-      else
-        b += 1
-      end
+      a += 1 if impulse == HIGH_PULSE
+      b += 1 if impulse != HIGH_PULSE
 
-      if flip_flops.key?(to)
-        next if impulse
-
-        if flip_flops[to]
-          # Low pulse
-          module_map[to].each do |w|
-            queue.push([w, LOW_PULSE, to])
-          end
-        else
-          # High pulse
-          module_map[to].each do |w|
-            queue.push([w, HIGH_PULSE, to])
-          end
-        end
-
-        flip_flops[to] = !flip_flops[to]
-      end
-
-      next unless conjunctions.key?(to)
-
-      conjunctions[to][from] = impulse
-
-      if conjunctions[to].values.all?
-        # Low pulse
-        module_map[to].each do |w|
-          queue.push([w, LOW_PULSE, to])
-        end
-      else
-        # High pulse
-        module_map[to].each do |w|
-          queue.push([w, HIGH_PULSE, to])
-          # Helper for part B
-          unless @conjunctions_first_high_pulse.key?(to)
-            @conjunctions_first_high_pulse[to] = iteration
-          end
-        end
-      end
+      process_flip_flops(to, impulse, flip_flops, queue)
+      process_conjunctions(to, from, impulse, conjunctions, iteration, queue)
     end
 
     [a, b]
+  end
+
+  def broadcast_initial_signal(queue)
+    @module_map['broadcaster'].each { |w| queue.push([w, LOW_PULSE, 'broadcaster']) }
+  end
+
+  def process_flip_flops(to, impulse, flip_flops, queue)
+    return unless flip_flops.key?(to) && !impulse
+
+    pulse_type = flip_flops[to] ? LOW_PULSE : HIGH_PULSE
+    @module_map[to].each { |w| queue.push([w, pulse_type, to]) }
+    flip_flops[to] = !flip_flops[to]
+  end
+
+  def process_conjunctions(to, from, impulse, conjunctions, iteration, queue)
+    return unless conjunctions.key?(to)
+
+    conjunctions[to][from] = impulse
+    pulse_type = conjunctions[to].values.all? ? LOW_PULSE : HIGH_PULSE
+
+    @module_map[to].each do |w|
+      queue.push([w, pulse_type, to])
+      @conjunctions_first_high_pulse[to] = iteration unless @conjunctions_first_high_pulse.key?(to) || pulse_type == LOW_PULSE
+    end
   end
 end
